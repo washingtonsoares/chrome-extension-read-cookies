050853f6eca02defe436e75e92056b67bc860715
Washington Soares <washington.soares@gamesys.co.uk>
2022-03-31 15:47:42 -0300

    feat(init): ༼ つ ◕_◕ ༽つ add initial files

---
+++ popup.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("control-row");
+const go = document.getElementById("go");
+const input = document.getElementById("input");
+const message = document.getElementById("message");
+
+// The async IIFE is necessary because Chrome <89 does not support top level await.
+(async function initPopupWindow() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (tab?.url) {
+    try {
+      let url = new URL(tab.url);
+      input.value = url.hostname;
+    } catch {}
+  }
+
+  input.focus();
+})();
+
+form.addEventListener("submit", handleFormSubmit);
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  clearMessage();
+
+  let url = stringToUrl(input.value);
+  if (!url) {
+    setMessage("Invalid URL");
+    return;
+  }
+
+  let message = await readDomainCookies(url.hostname);
+  setMessage(message);
+}
+
+function stringToUrl(input) {
+  // Start with treating the provided value as a URL
+  try {
+    return new URL(input);
+  } catch {}
+  // If that fails, try assuming the provided input is an HTTP host
+  try {
+    return new URL("http://" + input);
+  } catch {}
+  // If that fails ¯\_(ツ)_/¯
+  return null;
+}
+
+async function readDomainCookies(domain) {
+  let cookiesLength = 0;
+
+  try {
+    const cookies = await chrome.cookies.getAll({ domain });
+
+    cookiesLength = cookies.length;
+  } catch (error) {
+    return `Unexpected error: ${error.message}`;
+  }
+
+  return `Found ${cookiesLength} cookie(s).`;
+}
+
+function setMessage(str) {
+  message.textContent = str;
+  message.hidden = false;
+}
+
+function clearMessage() {
+  message.hidden = true;
+  message.textContent = "";
+}
